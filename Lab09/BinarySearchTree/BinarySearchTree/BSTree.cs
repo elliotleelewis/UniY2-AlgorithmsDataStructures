410353ab807b51164686f7051e0cba362a4d270e
Elliot Lewis <elliotleelewis@gmail.com>
2017-01-31 02:34:24 +0000

    Added Lab09 Tasks

---
+++ Lab09/BinarySearchTree/BinarySearchTree/BSTree.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BinarySearchTree
+{
+    internal class BSTree<T> : BinTree<T> where T : IComparable
+    {
+        public BSTree()
+        {
+            root = null;
+        }
+        public void InsertItem(T item)
+        {
+            _insertItem(item, ref root);
+        }
+        private void _insertItem(T item, ref Node<T> tree)
+        {
+            if (tree == null)
+                tree = new Node<T>(item);
+            else if (item.CompareTo(tree.Data) < 0)
+                _insertItem(item, ref tree.Left);
+            else if (item.CompareTo(tree.Data) > 0)
+                _insertItem(item, ref tree.Right);
+        }
+        public int Height()
+        {
+            return _height(root);
+        }
+        private int _height(Node<T> tree)
+        {
+            if (tree == null) {
+                return 0;
+            }
+            return 1 + Math.Max(_height(tree.Left), _height(tree.Right));
+        }
+        public bool Contains(T item)
+        {
+            return _contains(root, item);
+        }
+        private bool _contains(Node<T> tree, T item)
+        {
+            if (tree == null) {
+                return false;
+            }
+            if (tree.Data.Equals(item)) {
+                return true;
+            }
+            return _contains(tree.Left, item) || _contains(tree.Right, item);
+        }
+        public void RemoveItem(T item)
+        {
+            _removeItem(item, ref root);
+        }
+        private void _removeItem(T item, ref Node<T> tree)
+        {
+            if (tree == null) {
+                return;
+            }
+            if (item.CompareTo(tree.Data) < 0) {
+                _removeItem(item, ref tree.Left);
+            }
+            else if (item.CompareTo(tree.Data) > 0) {
+                _removeItem(item, ref tree.Right);
+            }
+            else if (tree.Left == null) {
+                tree = tree.Right;
+            }
+            else if (tree.Right == null) {
+                tree = tree.Left;
+            }
+            else {
+                var newRoot = leastItem(tree.Right);
+                tree.Data = newRoot;
+                _removeItem(newRoot, ref tree.Right);
+            }
+        }
+        private T leastItem(Node<T> tree)
+        {
+            return tree.Left == null ? tree.Data : leastItem(tree.Left);
+        }
+    }
+}
---
+++ Lab09/BinarySearchTree/BinarySearchTree/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BinarySearchTree
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var tree = new BSTree<int>();
+            tree.InsertItem(21);
+            tree.InsertItem(7);
+            tree.InsertItem(30);
+            tree.InsertItem(3);
+            tree.InsertItem(9);
+            tree.InsertItem(24);
+            tree.InsertItem(39);
+            tree.InsertItem(12);
+            Console.WriteLine(tree.InOrder());
+            Console.WriteLine(tree.Contains(21));
+            tree.RemoveItem(21);
+            Console.WriteLine(tree.Contains(21));
+            Console.WriteLine(tree.InOrder());
+            Console.ReadKey();
+        }
+    }
+}
